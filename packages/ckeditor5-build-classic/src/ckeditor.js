bd407c0f55f775867bfcdf6404c5a72da30ef05a
Luke Digby <laksmipati108@gmail.com>
2021-02-04 11:40:09 +0530

    Add toolbar config

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -56,4 +56,48 @@ Editor.builtinPlugins = [
 	TextTransformation
 ];
 
+// Editor configuration.
+Editor.defaultConfig = {
+  toolbar: {
+	items: [
+	  'heading',
+	  '|',
+	  'bold',
+	  'italic',
+	  'link',
+	  'bulletedList',
+	  'numberedList',
+	  '|',
+	  'indent',
+	  'outdent',
+	  '|',
+	  'imageUpload',
+	  'blockQuote',
+	  'insertTable',
+	  'mediaEmbed',
+	  'undo',
+	  'redo',
+	  '|',
+	  'htmlEmbed'
+	]
+  },
+  image: {
+	toolbar: [
+	  'imageStyle:full',
+	  'imageStyle:side',
+	  '|',
+	  'imageTextAlternative'
+	]
+  },
+  table: {
+	contentToolbar: [
+	  'tableColumn',
+	  'tableRow',
+	  'mergeTableCells'
+	]
+  },
+  // This value must be kept in sync with the language defined in webpack.config.js.
+  language: 'en'
+};
+
 export default Editor;
